89d0997840e047703fa5f444f13ba387864c8098
Wolfgang Popp <mail@wolfgang-popp.de>
2016-11-30 09:54:11 +0100

    Added GPLv3 License

--- src/main/java/de/unipassau/wolfgangpopp/xmlrss/wpprovider/WPProvider.java
+++ src/main/java/de/unipassau/wolfgangpopp/xmlrss/wpprovider/WPProvider.java
@@ -1,9 +1,32 @@
+/*
+ * XMLRSS - A Java Crypto Provider for Redactable Signatures and their
+ * XML Signature encoding.
+ *
+ * Copyright (c) 2016 Wolfgang Popp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.unipassau.wolfgangpopp.xmlrss.wpprovider;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.Provider;
 
+/**
+ * @author Wolfgang Popp
+ */
 public class WPProvider extends Provider{
 
     public WPProvider() {
