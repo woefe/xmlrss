aa621ae6e50ea4d5ac1d05427ce15cad847224a9
Wolfgang Popp <mail@wolfgang-popp.de>
2017-03-14 14:42:21 +0100

    PSRedactableXMLSignatureTest now uses hardcoded keys.
    Also added one more testcase.

--- src/test/java/de/unipassau/wolfgangpopp/xmlrss/wpprovider/psrss/PSRedactableXMLSignatureTest.java
+++ src/test/java/de/unipassau/wolfgangpopp/xmlrss/wpprovider/psrss/PSRedactableXMLSignatureTest.java
@@ -20,10 +20,11 @@
 
 package de.unipassau.wolfgangpopp.xmlrss.wpprovider.psrss;
 
-import de.unipassau.wolfgangpopp.xmlrss.wpprovider.xml.RedactableXMLSignature;
 import de.unipassau.wolfgangpopp.xmlrss.wpprovider.WPProvider;
+import de.unipassau.wolfgangpopp.xmlrss.wpprovider.xml.RedactableXMLSignature;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -33,29 +34,25 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.math.BigInteger;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  * @author Wolfgang Popp
  */
 public class PSRedactableXMLSignatureTest {
-    private static KeyPair keyPair;
+    private static final KeyPair keyPair;
 
     static {
         Security.insertProviderAt(new WPProvider(), 0);
-        KeyPairGenerator generator = null;
-        try {
-            generator = KeyPairGenerator.getInstance("PSRSS");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        generator.initialize(512);
-        keyPair = generator.generateKeyPair();
+        PSRSSPublicKey publicKey256 = new PSRSSPublicKey(new BigInteger("7249349928048807500024891411067629370056303429447255270046802991880425543412906735607605108373982421012500888307062421310001762155422489671132976679912849"));
+        PSRSSPrivateKey privateKey256 = new PSRSSPrivateKey(new BigInteger("7249349928048807500024891411067629370056303429447255270046802991880425543412734960638035580933850038621738468566657503090109097536944629352405060890801636"));
+        keyPair = new KeyPair(publicKey256, privateKey256);
     }
 
     @Test
@@ -74,13 +71,16 @@ public class PSRedactableXMLSignatureTest {
         sig.addPartSelector("#xpointer(id('a3'))");
         sig.sign();
 
+        NodeList nodeList = document.getElementsByTagNameNS(RedactableXMLSignature.XML_NAMESPACE, "Signature");
+        assertEquals(nodeList.getLength(), 1);
+        assertEquals(nodeList.item(0).getChildNodes().getLength(), 2);
 
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer trans = tf.newTransformer();
-        trans.setOutputProperty(OutputKeys.INDENT, "yes");
-        trans.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-        trans.transform(new DOMSource(document), new StreamResult(System.out));
-
+//        TransformerFactory tf = TransformerFactory.newInstance();
+//        Transformer trans = tf.newTransformer();
+//        trans.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "vehicles.dtd");
+////        trans.setOutputProperty(OutputKeys.INDENT, "yes");
+////        trans.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+//        trans.transform(new DOMSource(document), new StreamResult(new FileOutputStream("vehicles.sig.xml")));
     }
 
     @Test
@@ -102,6 +102,23 @@ public class PSRedactableXMLSignatureTest {
         sig.initVerify(keyPair.getPublic());
         sig.setDocument(document);
         assertTrue(sig.verify());
+    }
+
+    @Test
+    public void engineVerifyFail() throws Exception {
+        RedactableXMLSignature sig = RedactableXMLSignature.getInstance("XMLPSRSSwithPSA");
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setNamespaceAware(true);
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(new File("vehicles.sig.xml"));
+
+        NodeList nodeList = document.getElementsByTagName("Signature");
+        assertEquals(nodeList.getLength(), 1);
+
+        sig.initVerify(keyPair.getPublic());
+        sig.setDocument(document);
+        assertFalse(sig.verify());
     }
 
     @Test
